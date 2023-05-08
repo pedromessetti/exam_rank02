8c5395da38ccc8e019f31a88163ddb3765b0d13c
pedromessetti <pedromessetti@gmail.com>
2023-05-08 12:15:11 +0100

    add atoi

---
+++ level_2/ft_atoi.c
@@ -0,0 +1,25 @@
+/*
+Write a function that converts the string argument str to an integer (type int)
+and returns it.
+
+It works much like the standard atoi(const char *str) function, see the man.
+*/
+
+#include <stdio.h>
+
+int ft_atoi(const char *str) {
+    int sign = 1;
+    int i = 0;
+    int result = 0;
+    while(str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if(str[i] == '-')
+        sign = -1;
+    if (str[i] == '-' || str[i] == '+')
+        i++;
+    while (str[i] >= 48 && str[i] <= 57) {
+        result = result * 10 + str[i] - 48;
+        i++;
+    }
+    return (result*sign);
+}
