582dc8cb0a1356546c117a878c4595f1b3cce667
pedromessetti <pedromessetti@gmail.com>
2023-05-08 19:22:34 +0100

    add swap_bits

--- level_2/swap_bits.c
+++ level_2/swap_bits.c
@@ -3,6 +3,6 @@ Write a function that takes a byte, swaps its halves (like the example) and
 returns the result.
 */
 
-unsigned char swap_bits(unsigned char octet) { // 8 -- 0000 1000
+unsigned char swap_bits(unsigned char octet) {
 	return ((octet >> 4) | (octet << 4));
 }
