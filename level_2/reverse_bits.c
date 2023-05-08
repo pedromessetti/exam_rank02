3c8c1b93b913457b26ac940d01bbf0219ad16198
pedromessetti <pedromessetti@gmail.com>
2023-05-08 18:49:53 +0100

    add reverse_bits

---
+++ level_2/reverse_bits.c
@@ -0,0 +1,19 @@
+/*
+Write a function that takes a byte, reverses it, bit by bit (like the
+example) and returns the result.
+*/
+
+#include <stdio.h>
+
+unsigned char	reverse_bits(unsigned char octet)
+{
+	int		i = 8;
+	unsigned char	res = 0;
+
+	while (i > 0) {
+		res = res * 2 + (octet % 2);
+		octet = octet / 2;
+		i--; 
+	}
+	return (res);
+}
