9af65768a7b9f65bcc1216430fa345510280a511
pedromessetti <pedromessetti@gmail.com>
2023-07-14 16:46:51 +0100

    add flood_fill and expand_str

--- level_4/flood_fill.c
+++ level_4/flood_fill.c
@@ -1,3 +1,5 @@
+/*
+
 Assignment name  : flood_fill
 Expected files   : *.c, *.h
 Allowed functions: -
@@ -83,3 +85,25 @@ F00F000F
 F0FF000F
 FFF0000F
 $> 
+
+*/
+
+typedef struct  s_point
+{
+int           x;
+int           y;
+}               t_point;
+
+void fill(char**tab, t_point size, t_point curr, char to_fill) {
+	if (curr.x < 0 || curr.x >= size.x || curr.y < 0 || curr.y >= size.y || tab[curr.y][curr.x] != to_fill)
+		return ;
+	tab[curr.y][curr.x] = 'F';
+	fill(tab, size, (t_point){curr.x, curr.y+1}, to_fill);
+	fill(tab, size, (t_point){curr.x, curr.y-1}, to_fill);
+	fill(tab, size, (t_point){curr.x+1, curr.y}, to_fill);
+	fill(tab, size, (t_point){curr.x-1, curr.y}, to_fill);
+}
+
+void  flood_fill(char **tab, t_point size, t_point begin) {
+	fill(tab, size, begin, tab[begin.y][begin.x]);
+}
