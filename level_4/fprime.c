c2420c7138112960e9d231359714cadd4d74d9fc
pedromessetti <pedromessetti@gmail.com>
2023-07-17 11:05:24 +0100

    add fprime

--- level_4/fprime.c
+++ level_4/fprime.c
@@ -1,3 +1,5 @@
+/*
+
 Assignment name  : fprime
 Expected files   : fprime.c
 Allowed functions: printf, atoi
@@ -31,3 +33,29 @@ $> ./fprime | cat -e
 $
 $> ./fprime 42 21 | cat -e
 $
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int ac, char **av) {
+    if (ac == 2) {
+        if (!*av[1] || av[1][0] == '-') return printf("\n");
+        int n = atoi(av[1]);
+        if (n == 1) printf("1");
+        
+        int i = 1;
+        while(n >= ++i) {
+            if (n % i == 0) {
+                printf("%d", i);
+                if (n == i) break;
+                printf("*");
+                n /= i;
+                i = 1;
+            }
+        }
+    }
+    printf("\n");
+    return 0;
+}
